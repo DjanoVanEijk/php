4a9668161f4ee015eb944d0532923b1193d62034
DjanoVanEijk <9028315@student.zadkine.nl>
2026-07-02 10:43:56 +0200

    Update nav.php

--- p4/hoofdopdracht/includes/nav.php
+++ p4/hoofdopdracht/includes/nav.php
@@ -1,7 +1,14 @@
 
 <nav>
     <ul>
-        <li><a href="pages/status_check.php">Inloggen/Uitloggen</a></li>
-        <li><a href="pages/huiswerk.php">Huiswerk</a></li>
+        <li><a href="/php/p4/hoofdopdracht/index.php">Home</a></li>
+        <li><a href="/php/p4/hoofdopdracht/pages/huiswerk.php">Huiswerk</a></li>
+
+        <?php if (!empty($_SESSION['userid'])): ?>
+            <li><a href="/php/p4/hoofdopdracht/pages/logout.php">Uitloggen</a></li>
+        <?php else: ?>
+            <li><a href="/php/p4/hoofdopdracht/pages/login.php">Inloggen</a></li>
+            <li><a href="/php/p4/hoofdopdracht/pages/register.php">Registreren</a></li>
+        <?php endif; ?>
     </ul>
 </nav>
